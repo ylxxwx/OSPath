d26f16f08b01c446e6a139c78769e7f42a351576
ylxxwx <ylxxwx@gmail.com>
2022-12-12 10:19:49 -0500

    Cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,9 +1,3 @@
-#!bin/bash
+#!/bin/bash
 
-mexec()
-{ 
-  export THE_COMMAND=$@
-  find . -type d -maxdepth 1 -mindepth 1 -print0 | xargs -0 -I{} zsh -c 'cd "{}" && echo "{}" && echo "$('$THE_COMMAND')" && echo -e'
-}
-
-mexec make clean
+for d in ./*/ ; do (cd "$d" && make clean); done
